8ecba5382cffb362c825307ec4d63d79b940894d
harshpratap317 <hpratap404@gmail.com>
2026-09-19 15:20:35 +0530

    Prepare backend for Vercel

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,24 @@
 require("dotenv").config();
 
+const express = require("express");
 const cors = require("cors");
-
 const connectDB = require("./config/db");
 
+const taskRoutes = require("./routes/taskRoutes");
 const authRoutes = require("./routes/authRoutes");
-// app.use(express.json());
-const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
-
-const taskRoutes = require("./routes/taskRoutes");
-
-app.use(express.json());
 app.use(cors());
-
+app.use(express.json());
 
 connectDB();
 
 app.get("/", (req, res) => {
-    res.send("Task manager Backend is running");
+  res.send("Task Manager Backend is running");
 });
 
 app.use("/api/tasks", taskRoutes);
-
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-});
+module.exports = app;
